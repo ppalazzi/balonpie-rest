db9331744d6ce6927f203a994bc3e90a571f6889
Pablo Palazzi <ppalazzi@starmount.com>
2019-02-03 15:02:05 -0300

    solucion temporal

--- src/main/java/com/palazzisoft/balonpie/service/service/impl/PartidoServiceImpl.java
+++ src/main/java/com/palazzisoft/balonpie/service/service/impl/PartidoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.palazzisoft.balonpie.service.service.impl;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,16 @@ public class PartidoServiceImpl implements PartidoService {
 
 	@Override
 	public void jugarPartido(Partido partido) {
-		// TODO pensar una buena implementacion	
+		// TODO pensar una buena implementacion
+
+        Random rand = new Random();
+
+		int golesLocal = rand.nextInt(5);
+		int golesVisitante = rand.nextInt(4);
+
+		partido.setGolesLocal(golesLocal);
+		partido.setGolesVisitante(golesVisitante);
+
 		partido.setJugado(true);
 	}
 
